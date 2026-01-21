0d95f050a333eb3d44bb7f0e996ed49f0817380e
Emlyn Matheson <emlyn@emlyn.xyz>
2026-01-21 14:31:20 +1100

    feat: Add non-interactive label widget

---
+++ src/widgets/label.cpp
@@ -0,0 +1,14 @@
+#include "./widgets.hpp"
+#include "gtk/gtk.h"
+
+GtkWidget* label_widget(char* label_content) {
+	GtkWidget* wrapper = gtk_vbox_new(FALSE, 0);
+		
+	GtkWidget* label = gtk_label_new(label_content);
+	PangoFontDescription* font_desc = pango_font_description_from_string(FONT_10);
+	gtk_widget_modify_font(label, font_desc);
+	pango_font_description_free(font_desc);
+		
+	gtk_box_pack_start(GTK_BOX(wrapper), label, TRUE, TRUE, 5*SCALE);
+	return wrapper;
+}
